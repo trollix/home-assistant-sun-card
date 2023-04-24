785edd25d007a1da2ea0a053bd9e73ffc35a4afe
trollix <dr.chandra@laposte.net>
2023-04-24 11:34:33 +0200

    release

--- src/cardContent.ts
+++ src/cardContent.ts
@@ -112,19 +112,21 @@ export class SunCardContent {
               <stop offset="${data?.sunPercentOverHorizon ?? 0}%" style="stop-color:rgb(0,0,0,0);stop-opacity:1" />
             </linearGradient>
           -->
-            // aube
+            // aube - dawn
             <linearGradient id="${dawnID}" x1="0%" y1="0%" x2="100%" y2="0%">
-              <stop offset="50%" style="stop-color:#8ebeeb;stop-opacity:1" />
-              <stop offset="${data?.dawnProgressPercent ?? 0}%" style="stop-color:#393b78;stop-opacity:1" />
+              <stop offset="50%" style="stop-color:#393b78;stop-opacity:1" />
+              <stop offset="${data?.dawnProgressPercent ?? 0}%" style="stop-color:#8ebeeb;stop-opacity:1" />
               <stop offset="${data?.dawnProgressPercent ?? 0}%" style="stop-color:rgb(0,0,0,0);stop-opacity:1" />
             </linearGradient>
             
+            // jour - day
             <linearGradient id="${dayID}" x1="0%" y1="0%" x2="100%" y2="0%">
               <stop offset="70%" style="stop-color:#8ebeeb;stop-opacity:1" />
               <stop offset="${data?.dayProgressPercent ?? 0}%" style="stop-color:#393b78;stop-opacity:1"  />
               <stop offset="${data?.dayProgressPercent ?? 0}%" style="stop-color:rgb(0,0,0,0);stop-opacity:1" />
             </linearGradient>
             
+            // crépuscule - dusk
             <linearGradient id="${duskID}" x1="0%" y1="0%" x2="100%" y2="0%">
               <stop offset="0%" style="stop-color:#393b78;stop-opacity:1" />
               <stop offset="${data?.duskProgressPercent ?? 0}%" style="stop-color:#393b78;stop-opacity:1" />
