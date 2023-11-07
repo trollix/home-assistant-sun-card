bda84ba0534eb92b843d28d0f98e3af616ac51a2
trollix <dr.chandra@laposte.net>
2023-11-07 16:04:38 +0100

    release 0.72.14

--- src/types/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ESunCardErrors = void 0;
-var ESunCardErrors;
-(function (ESunCardErrors) {
-    ESunCardErrors["SunIntegrationNotFound"] = "SunIntegrationNotFound";
-})(ESunCardErrors = exports.ESunCardErrors || (exports.ESunCardErrors = {}));
