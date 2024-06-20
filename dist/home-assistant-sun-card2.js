9cc5bdd02aae847e6f9ae9636ab676beb73728cf
trollix <dr.chandra@laposte.net>
2024-06-20 11:37:24 +0200

    release 0.75.44

--- dist/home-assistant-sun-card2.js
+++ dist/home-assistant-sun-card2.js
@@ -1136,7 +1136,7 @@ ESunCardErrors = /*#__PURE__*/function (ESunCardErrors) {
 }(ESunCardErrors || {});
 
 var name = "homeassistant-sun-card-2";
-var version = "0.75.43";
+var version = "0.75.44";
 
 console.info("%c  ".concat(name.toUpperCase(), "  %c  Version ").concat(version, "  "), 'color: white; font-weight: bold; background: crimson', 'color: #000; font-weight: bold; background: #ddd');
 var SunCard = _decorate([e$1('sun-card')], function (_initialize, _LitElement) {
